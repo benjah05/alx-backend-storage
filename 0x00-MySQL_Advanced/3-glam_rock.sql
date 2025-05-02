ee4a13f7bf8c95c6f87773c2aa4b1105c248808a
benjah05 <senzelebenjamine@gmail.com>
2025-05-02 01:17:46 +0000

    band lifespan

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- old school band
+-- lists all bands will glam rock as their main style
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
